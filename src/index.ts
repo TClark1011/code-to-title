8485fc5fe934a26f8aa8f1c724e481c3f22d865d
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-02-26 08:33:04 +1100

    tweaked options

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 interface CodeToTitleOptions {
-	replaceWithSpace?: string[];
-	breakupCamelCase?: boolean;
+	replaceWithSpace?: string | string[];
+	breakupCamelCase?: string | boolean;
 	capitalizeWords?: boolean;
 }
 
@@ -10,10 +10,12 @@ interface CodeToTitleOptions {
  * @param {string} input The input string that will be formatted
  * @param {CodeToTitleOptions} options Options indicating how the input
  * string will be formatted
- * @param {string[]} [options.replaceWithSpace=["-","_"]] A list of strings,
- * all occurrences of which in the input string will be replaced with a space
- * @param {boolean} [options.breakupCamelCase=true] Weather or not to break up instances
- * of camel case (eg; 'camelCase') with spaces.
+ * @param {string | string[]} [options.replaceWithSpace=["-","_"]] A string or
+ * list of strings that should be replaced with spaces.
+ * @param {string | boolean} [options.breakupCamelCase=" "] How to break up
+ * instances of camel case naming (eg; 'camelCase'). If passed a string, that
+ * string will be used to breakup camel cases. If passed 'false', camel cases will
+ * not be broken up. If passed 'true', camel cases will be broken up with a space.
  * @param {boolean} [options.capitalizeWords=true] Weather or not to capitalize the first
  * letter of every word.
  * @returns {string} The input string, formatted for use in a title
@@ -22,19 +24,24 @@ const codeToTitle = (
 	input: string,
 	{
 		replaceWithSpace = ["-", "_"],
-		breakupCamelCase = true,
+		breakupCamelCase = " ",
 		capitalizeWords = true,
 	}: CodeToTitleOptions = {}
 ): string => {
 	let result = input;
 
-	for (const item of replaceWithSpace) {
+	const replaceWithSpaceArr =
+		typeof replaceWithSpace === "string"
+			? [replaceWithSpace]
+			: replaceWithSpace;
+	for (const item of replaceWithSpaceArr) {
 		result = result.replace(new RegExp(item, "g"), " ");
 	}
 
 	if (breakupCamelCase) {
+		const breakupWith = breakupCamelCase === true ? " " : breakupCamelCase;
 		result = result.replace(/\B(?![a-z]+)[A-Z]/g, (match, index: number) =>
-			index > 0 ? " " + match : match
+			index > 0 ? breakupWith + match : match
 		);
 	}
 

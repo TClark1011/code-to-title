ddfce6165f1a7eb729955fbf5a989cd226082c46
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-02-26 08:01:51 +1100

    fixed jest config for use with 'ts-jest'

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
 	"testTimeout": 10000,
 	"testPathIgnorePatterns": ["/node_modules/", "/build/"],
 	"transform": {
-		".(ts|tsx)": "<rootDir>/node_modules/ts-jest/preprocessor.js",
+		".(ts|tsx)": "ts-jest",
 	},
 	"testRegex": "(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$",
 	"moduleFileExtensions": ["ts", "tsx", "js"],
